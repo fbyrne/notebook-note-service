4f16a20b05ef1c7146cff9cb6e92d96c8f3134d5
Fergus Byrne <fergus.byrne@gmail.com>
2020-08-14 00:20:54 +0100

    Initial commit of project

---
+++ src/main/java/com/fbyrne/notebook/notebookproject/NotebookProjectApplication.java
@@ -0,0 +1,13 @@
+package com.fbyrne.notebook.notebookproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotebookProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotebookProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fbyrne/notebook/notebookproject/NotebookProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fbyrne.notebook.notebookproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotebookProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
